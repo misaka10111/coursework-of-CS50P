6fd10fd2d0ec5a7cd71a05331551fe31adf3a60e
Jack <jiaqizhao2074@gmail.com>
2026-02-06 00:35:31 +0000

    final-project: skeleton

---
+++ Final_Project/project.py
@@ -0,0 +1,15 @@
+def main():
+    ...
+
+def function1():
+    ...
+
+def function2():
+    ...
+
+def function3():
+    ...
+
+
+if __name__ == "__main__":
+    main()
---
+++ Final_Project/test_project.py
@@ -0,0 +1,12 @@
+import pytest
+import project
+
+
+def test_function1():
+    ...
+
+def test_function2():
+    ...
+
+def test_function3():
+    ...
